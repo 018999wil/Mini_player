8c09c7e19639ed3eefcd23fb983db9578467650f
진현민 <os13060@naver.com>
2023-06-04 20:44:14 +0900

    Update Mini_player.py

--- Mini_player.py
+++ Mini_player.py
@@ -23,10 +23,8 @@ This module contains a bare-bones VLC player class to play videos.
 Author: Saveliy Yusufov, Columbia University, sy2685@columbia.edu
 Date: 25 January 2019
 """
-
 import os
 import sys
-import queue
 import platform
 
 from PyQt5 import QtWidgets, QtGui, QtCore
@@ -37,7 +35,7 @@ class MiniPlayer(QtWidgets.QMainWindow):
     """Stripped-down PyQt5-based media player class to sync with "master" video.
     """
 
-    def __init__(self, data_queue, master=None):
+    def __init__(self, master=None):
         QtWidgets.QMainWindow.__init__(self, master)
         self.setWindowTitle("Mini Player")
         self.statusbar = self.statusBar()
@@ -46,24 +44,23 @@ class MiniPlayer(QtWidgets.QMainWindow):
         # Create a basic vlc instance
         self.instance = vlc.Instance()
 
-        self.media = None
+        self.video_media = None
+        self.audio_media = None
 
-        # Create an empty vlc media player
-        self.mediaplayer = self.instance.media_player_new()
+        # Create an empty vlc media player for video
+        self.video_mediaplayer = self.instance.media_player_new()
+
+        # Create an empty vlc media player for audio
+        self.audio_mediaplayer = self.instance.media_player_new()
 
         self.init_ui()
-        self.open_file()
+        self.open_files()
 
         self.timer = QtCore.QTimer(self)
         self.timer.setInterval(10)
         self.timer.timeout.connect(self.update_ui)
 
-        self.data_queue = data_queue
         self.timer.start()
-
-        # Key event filter
-        self.key_event_filter = KeyPressEventFilter(self)
-        self.installEventFilter(self.key_event_filter)
 
     def init_ui(self):
         """Set up the user interface
@@ -81,95 +78,80 @@ class MiniPlayer(QtWidgets.QMainWindow):
         self.palette.setColor(QtGui.QPalette.Window, QtGui.QColor(0, 0, 0))
         self.videoframe.setPalette(self.palette)
         self.videoframe.setAutoFillBackground(True)
-
+        
         self.vboxlayout = QtWidgets.QVBoxLayout()
         self.vboxlayout.addWidget(self.videoframe)
         self.widget.setLayout(self.vboxlayout)
 
-    def open_file(self):
-        """Open a media file in a MediaPlayer
+        self.setWindowFlags(QtCore.Qt.WindowStaysOnTopHint) #플레이어윈도우 맨위로
+
+        self.stacked_layout.addWidget(self.videoframe)
+        self.widget.setLayout(self.stacked_layout)
+
+    def open_files(self):
+        """Open media files in media players
         """
-        dialog_txt = "Choose Media File"
-        filename = QtWidgets.QFileDialog.getOpenFileName(self, dialog_txt, os.path.expanduser('~'))
-        if not filename[0]:
+        dialog_txt = "Choose Video File"
+        filename_video = QtWidgets.QFileDialog.getOpenFileName(self, dialog_txt, os.path.expanduser('~'))
+        if not filename_video[0]:
+            return
+
+        dialog_txt = "Choose Audio File"
+        filename_audio = QtWidgets.QFileDialog.getOpenFileName(self, dialog_txt, os.path.expanduser('~'))
+        if not filename_audio[0]:
             return
 
         # getOpenFileName returns a tuple, so use only the actual file name
-        self.media = self.instance.media_new(filename[0])
+        self.video_media = self.instance.media_new(filename_video[0])
+        self.audio_media = self.instance.media_new(filename_audio[0])
 
-        # Put the media in the media player
-        self.mediaplayer.set_media(self.media)
+        # Put the media in the media players
+        self.video_mediaplayer.set_media(self.video_media)
+        self.audio_mediaplayer.set_media(self.audio_media)
 
-        # Parse the metadata of the file
-        self.media.parse()
+        # Parse the metadata of the files
+        self.video_media.parse()
+        self.audio_media.parse()
 
         # Set the title of the track as the window title
-        self.setWindowTitle("{}".format(self.media.get_meta(0)))
+        self.setWindowTitle("{}".format(self.video_media.get_meta(0)))
 
-        # The media player has to be 'connected' to the QFrame (otherwise the
-        # video would be displayed in it's own window). This is platform
-        # specific, so we must give the ID of the QFrame (or similar object) to
-        # vlc. Different platforms have different functions for this
+        # The media players have to be 'connected' to the respective QFrames
+        # This is platform specific, so we must give the IDs of the QFrames (or similar objects) to vlc
         if platform.system() == "Linux":  # for Linux using the X Server
-            self.mediaplayer.set_xwindow(int(self.videoframe.winId()))
+            self.video_mediaplayer.set_xwindow(int(self.videoframe.winId()))
+            self.audio_mediaplayer.set_xwindow(int(self.videoframe.winId()))
         elif platform.system() == "Windows":  # for Windows
-            self.mediaplayer.set_hwnd(int(self.videoframe.winId()))
+            self.video_mediaplayer.set_hwnd(int(self.videoframe.winId()))
+            self.audio_mediaplayer.set_hwnd(int(self.videoframe.winId()))
         elif platform.system() == "Darwin":  # for MacOS
-            self.mediaplayer.set_nsobject(int(self.videoframe.winId()))
+            self.video_mediaplayer.set_nsobject(int(self.videoframe.winId()))
+            self.audio_mediaplayer.set_nsobject(int(self.videoframe.winId()))
 
-        # Start playing the video as soon as it loads
-        self.mediaplayer.play()
+        # Start playing the video and audio as soon as they load
+        self.video_mediaplayer.play()
+        self.audio_mediaplayer.play()
 
     def update_ui(self):
         self.update_statusbar()
 
-        try:
-            val = self.data_queue.get(block=False)
-        except queue.Empty:
-            return
+        if self.video_mediaplayer.get_state() == vlc.State.Ended:
+            self.video_mediaplayer.stop()
+            self.video_mediaplayer.play()
 
-
-        val = int(val)
-        if val != self.mediaplayer.get_time():
-            self.mediaplayer.set_time(val)
+        if self.audio_mediaplayer.get_state() == vlc.State.Ended:
+            self.audio_mediaplayer.stop()
+            self.audio_mediaplayer.play()
 
     def update_statusbar(self):
         mtime = QtCore.QTime(0, 0, 0, 0)
-        time = mtime.addMSecs(self.mediaplayer.get_time())
-        self.statusbar.showMessage(time.toString())
+        time_video = mtime.addMSecs(self.video_mediaplayer.get_time())
+        time_audio = mtime.addMSecs(self.audio_mediaplayer.get_time())
 
-
-
-
-class KeyPressEventFilter(QtCore.QObject):
-    """Key press event filter to handle keyboard events"""
-
-    def __init__(self, parent=None):
-        super().__init__(parent)
-
-    def eventFilter(self, obj, event):
-        if event.type() == QtCore.QEvent.KeyPress:
-            if event.key() == QtCore.Qt.Key_P:
-                # play/pause
-                if obj.mediaplayer.is_playing():
-                    obj.mediaplayer.pause()
-                else:
-                    obj.mediaplayer.play()
-            elif event.key() == QtCore.Qt.Key_S:
-                # 멈춤
-                obj.mediaplayer.stop()
-            elif event.key() == QtCore.Qt.Key_Left:
-                # 0.5배속
-                obj.mediaplayer.set_rate(obj.mediaplayer.get_rate() * 0.5)
-            elif event.key() == QtCore.Qt.Key_Right:
-                # 2배속
-                obj.mediaplayer.set_rate(obj.mediaplayer.get_rate() * 2)
-            else:
-                return super().eventFilter(obj, event)
-
-            return True
-
-        return super().eventFilter(obj, event)
+        if self.video_mediaplayer.is_playing():
+            self.statusbar.showMessage("Video: " + time_video.toString())
+        elif self.audio_mediaplayer.is_playing():
+            self.statusbar.showMessage("Audio: " + time_audio.toString())
 
 
 def main():
@@ -177,9 +159,7 @@ def main():
     """
     app = QtWidgets.QApplication(sys.argv)
 
-    data_queue = queue.Queue()
-
-    player = MiniPlayer(data_queue)
+    player = MiniPlayer()
     player.show()
     player.resize(480, 480)
 
